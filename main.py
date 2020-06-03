0e99224c650c972465fe543886fd030da834cf4e
Matthew <matt.oliver.2004@gmail.com>
2020-06-03 15:27:28 -0400

    Cleaned up code, added more comments

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import pandas as pd
-from matplotlib import pyplot as plt
-import numpy as np
-from datetime import datetime
-import sqlite3
-from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets, QtCore
+from matplotlib import pyplot as plt
+from datetime import datetime
+import numpy as np
+import sqlite3
 import sys
+
+
+# Score Processing Functions
 
 
 # Sort raw data into separate flights | Returns: list of all flights/scores
@@ -123,13 +125,85 @@ def cleanUpTime(rawlist):
     return cleanList, flightScore, averageFlightScore, arrowStatsStr
 
 
+# Combine all results into one data set | Returns: arrowsShot, averageFlightScore, individualScores
+def allTimeResults(results):
+    arrowsShot = 0
+    averageFlightScore = 0
+    tens = 0
+    nines = 0
+    eights = 0
+    sevens = 0
+    sixes = 0
+    fives = 0
+    fours = 0
+    threes = 0
+    twos = 0
+    ones = 0
+    zeros = 0
+    individualScores = []
+    for day in results:
+        arrowsShot += int(day[1])
+        averageFlightScore += int(day[2])
+        tens += int(day[3])
+        nines += int(day[4])
+        eights += int(day[5])
+        sevens += int(day[6])
+        sixes += int(day[7])
+        fives += int(day[8])
+        fours += int(day[9])
+        threes += int(day[10])
+        twos += int(day[11])
+        ones += int(day[12])
+        zeros += int(day[13])
+    individualScores.append(tens)
+    individualScores.append(nines)
+    individualScores.append(eights)
+    individualScores.append(sevens)
+    individualScores.append(sixes)
+    individualScores.append(fives)
+    individualScores.append(fours)
+    individualScores.append(threes)
+    individualScores.append(twos)
+    individualScores.append(ones)
+    individualScores.append(zeros)
+    averageFlightScore = averageFlightScore/len(results)
+    return arrowsShot, averageFlightScore, individualScores
+
+
+# Make pie graph based on inputted data | Returns: nothing
+def pieGraphScores(scores, title="", show=True, save=False):
+
+    # Define the label stuffs
+    labelStr = '10', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'
+    labelNum = np.array(scores)
+    labelNumPercent = 100.*labelNum/labelNum.sum()
+
+    # Combine labelStr with labelNum to go into legend
+    label = ['{0} - {1:1.2f} %'.format(i, j) for i, j in zip(labelStr, labelNumPercent)]
+
+    # Setup the pie graph
+    fig1, axl = plt.subplots()
+    axl.pie(labelNum, shadow=False, startangle=90, counterclock=True)
+    axl.axis('equal')
+
+    # Setup legend and title
+    plt.legend(label, title='Scores', bbox_to_anchor=(0.15, 1.), fontsize=8)
+    plt.title(title, loc='center')
+
+    if save == True:
+        plt.savefig('piechart'+datetime.today().strftime('%m-%d-%H-%M')+".png")
+    if show == True:
+        plt.show()
+
+
+# Score Processing Functions Over
+
+
+# Database Functions Below
+
+
 # Save data (date, arrows shot, average flight score, and individual arrow scores) to database | Returns: nothing
 def saveData(data):
-
-    # Read raw data from text file
-    #scoreFile = open("scores.txt")
-    #scores = scoreFile.read()
-    #scoreFile.close()
 
     # Run code to clean up/format data
     cleanList, flightScore, averageFlightScore, arrowStatsStr = cleanUpTime(data)
@@ -178,99 +252,30 @@ def retrieveData(gui=False):
     return results
 
 
-def deleteDataDatabase(date):
+# Deletes selected data from database
+def deleteData(date):
+
+    # Connect to database
     connection = sqlite3.connect('saveData.db')
     cursor = connection.cursor()
 
+    # Delete data
     print("DELETE FROM data WHERE date = "+date)
     cursor.execute("DELETE FROM data WHERE date = '"+date+"'")
 
+    # Close connection
     connection.commit()
     cursor.close()
     connection.close()
 
 
-# Combine all results into one data set | Returns: arrowsShot, averageFlightScore, individualScores
-def allTimeResults(results):
-    arrowsShot = 0
-    averageFlightScore = 0
-    tens = 0
-    nines = 0
-    eights = 0
-    sevens = 0
-    sixes = 0
-    fives = 0
-    fours = 0
-    threes = 0
-    twos = 0
-    ones = 0
-    zeros = 0
-    individualScores = []
-    for day in results:
-        arrowsShot += int(day[1])
-        averageFlightScore += int(day[2])
-        tens += int(day[3])
-        nines += int(day[4])
-        eights += int(day[5])
-        sevens += int(day[6])
-        sixes += int(day[7])
-        fives += int(day[8])
-        fours += int(day[9])
-        threes += int(day[10])
-        twos += int(day[11])
-        ones += int(day[12])
-        zeros += int(day[13])
-    individualScores.append(tens)
-    individualScores.append(nines)
-    individualScores.append(eights)
-    individualScores.append(sevens)
-    individualScores.append(sixes)
-    individualScores.append(fives)
-    individualScores.append(fours)
-    individualScores.append(threes)
-    individualScores.append(twos)
-    individualScores.append(ones)
-    individualScores.append(zeros)
-    averageFlightScore = averageFlightScore/len(results)
-    return arrowsShot, averageFlightScore, individualScores
+# Database Functions Over
 
 
-#totalArrowsShot, totalAverageFlightScore, totalIndividualScore = allTimeResults(retrieveData())
-#print("totalArrowsShot: "+str(totalArrowsShot)+", totalAverageFlightScore: "+str(totalAverageFlightScore)+", totalIndividualScore: "+str(totalIndividualScore))
-# saveData()
+# GUI Functions Below
 
 
-# Make pie graph based on inputted data | Returns: nothing
-def pieGraphScores(scores, title="", show=True, save=False):
-
-    # Define the label stuffs
-    labelStr = '10', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'
-    labelNum = np.array(scores)
-    labelNumPercent = 100.*labelNum/labelNum.sum()
-
-    # Combine labelStr with labelNum to go into legend
-    label = ['{0} - {1:1.2f} %'.format(i, j) for i, j in zip(labelStr, labelNumPercent)]
-
-    # Setup the pie graph
-    fig1, axl = plt.subplots()
-    axl.pie(labelNum, shadow=False, startangle=90, counterclock=True)
-    axl.axis('equal')
-
-    # Setup legend and title
-    plt.legend(label, title='Scores', bbox_to_anchor=(0.15, 1.), fontsize=8)
-    plt.title(title, loc='center')
-
-    if save == True:
-        plt.savefig('piechart'+datetime.today().strftime('%m-%d-%H-%M')+".png")
-    if show == True:
-        plt.show()
-
-
-#totalArrowsShot, totalAverageFlightScore, totalIndividualScore = allTimeResults(retrieveData())
-#print(totalIndividualScore)
-#pieGraphScores(totalIndividualScore, title="All Available Data", show=True, save=False)
-
-currentlySelectedItem = []
+# Keep track of item currently selected on GUI list
 def itemClicked(item):
     modded = item.text().replace("(", "")
     modded = modded.replace(")", "")
@@ -278,10 +283,13 @@ def itemClicked(item):
     modded = modded.split(", ")
     global currentlySelectedItem
     currentlySelectedItem = modded
+
+    # Update text under list
     data = ("Arrows shot: " + str(currentlySelectedItem[1]) + ", Average flight score: " + str(currentlySelectedItem[2]))
     outputText.setText(data)
 
 
+# Calls pieGraphScores() for GUI
 def guiPieGraphScores():
     global currentlySelectedItem
     scores = []
@@ -293,6 +301,7 @@ def guiPieGraphScores():
         pieGraphScores(scores, title=str(currentlySelectedItem[0]), save=False)
 
 
+# Calls saveData() for GUI
 def guiSaveData():
     if rawScoreData.text() == "":
         print("No text provided")
@@ -301,12 +310,16 @@ def guiSaveData():
     retrieveData(gui=True)
 
 
+# Calls deleteData() for GUI
 def guiDeleteSelectedData():
     global currentlySelectedItem
     date = str(currentlySelectedItem[0])
     print("Deleting date: "+date)
-    deleteDataDatabase(date)
+    deleteData(date)
     retrieveData(gui=True)
+
+
+# Actual GUI Stuff Below
 
 
 app = QApplication(sys.argv)
@@ -339,11 +352,12 @@ savePieGraph = QtWidgets.QCheckBox(centralwidget)
 savePieGraph.setGeometry(QtCore.QRect(480, 140, 141, 41))
 savePieGraph.setObjectName("savePieGraph")
 
-deleteData = QtWidgets.QPushButton(centralwidget)
-deleteData.setGeometry(QtCore.QRect(310, 200, 141, 41))
-deleteData.setObjectName("deleteData")
-deleteData.clicked.connect(lambda: guiDeleteSelectedData())
+deleteDataButton = QtWidgets.QPushButton(centralwidget)
+deleteDataButton.setGeometry(QtCore.QRect(310, 200, 141, 41))
+deleteDataButton.setObjectName("deleteDataButton")
+deleteDataButton.clicked.connect(lambda: guiDeleteSelectedData())
 
+currentlySelectedItem = []
 databaseInfoView = QtWidgets.QListWidget(centralwidget)
 databaseInfoView.setGeometry(QtCore.QRect(20, 70, 285, 180))
 databaseInfoView.setObjectName("databaseInfoView")
@@ -353,7 +367,6 @@ databaseInfoView.itemClicked.connect(itemClicked)
 outputText = QtWidgets.QLabel(centralwidget)
 outputText.setGeometry(30, 250, 300, 100)
 outputText.setObjectName("outputText")
-#outputText.setText("No Output Generated")
 
 MainWindow.setCentralWidget(centralwidget)
 menubar = QtWidgets.QMenuBar(MainWindow)
@@ -365,11 +378,11 @@ statusbar.setObjectName("statusbar")
 MainWindow.setStatusBar(statusbar)
 
 _translate = QtCore.QCoreApplication.translate
-MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+MainWindow.setWindowTitle(_translate("MainWindow", "ArcheryStats"))
 saveDataButton.setText(_translate("MainWindow", "Save Data to Database"))
 retrieveDataButton.setText(_translate("MainWindow", "Refresh Database View"))
 generatePieGraph.setText(_translate("MainWindow", "Generate Pie Graph"))
-deleteData.setText(_translate("MainWindow", "Delete Selected Data"))
+deleteDataButton.setText(_translate("MainWindow", "Delete Selected Data"))
 savePieGraph.setText(_translate("MainWindow", "Save Pie Graph"))
 
 MainWindow.show()
